98a2c47d75da023cc0cf47335fdf6d68136ce2b5
Ayush Kumar Yadav <69889382+zaCKoZAck0@users.noreply.github.com>
2022-10-09 18:11:46 +0530

    Delete Trust Plex directory

--- Trust Plex/server/routes/search.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { movieSearch } = require("../controllers/search");
-const auth = require("../utils/jwt");
-const router = express.Router();
-
-router.get("/:query", movieSearch);
-
-module.exports = router;
--- Trust Plex/server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { userLogin, userSignup } = require("../controllers/user");
-const router = express.Router();
-
-router.post("/login", userLogin);
-router.post("/signup", userSignup);
-
-module.exports = router;
